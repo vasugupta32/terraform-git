32f8bf33f471e7d7538f8a825d1d6ea4dceb8e95
vasuguptaxperi <vasu.gupta@xperi.com>
2023-10-30 22:50:47 +0530

    tetsing modules

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,3 @@
-
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAURFWCS3UX5DZ5QPZ"
-  secret_key = "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"
-}
-
-resource "aws_instance" "myec2" {
-  ami           = "ami-0d2017e886fc2c0ab"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "server1"
-  }
+module "demomodule" {
+  source = "git::https://github.com/vasugupta32/terraform-git.git"
 }
